c6f80a46d8af09a871f58dc0cd7d627bcc28f058
Radojko Drcelic <rdrcelic@pivotal.io>
2018-03-15 17:13:34 +0100

    initial commit

---
+++ src/main/java/com/rdrce/troskovi/TroskoviApplication.java
@@ -0,0 +1,12 @@
+package com.rdrce.troskovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroskoviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroskoviApplication.class, args);
+	}
+}
